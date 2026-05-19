bdb7203d8a58da2173221a4949ca7c2589a78456
Maneesh Kumar M <maneeshkallada37@gmail.com>
2026-05-19 20:49:20 +0530

    Optimize SQLite performance with WAL mode and explicit start command

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput && gunicorn goldenfork.wsgi --workers 4 --threads 4
+web: python manage.py collectstatic --noinput && gunicorn goldenfork.wsgi --workers 2 --threads 4
--- goldenfork/settings.py
+++ goldenfork/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,
+            'init_command': 'PRAGMA journal_mode=WAL; PRAGMA synchronous=NORMAL;',
+        }
     }
 }
 
@@ -125,7 +129,6 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
