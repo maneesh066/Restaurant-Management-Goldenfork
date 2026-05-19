f941105e9fab5cc0c6df0427a4fd7a8b288d02d6
Maneesh Kumar M <maneeshkallada37@gmail.com>
2026-05-19 20:40:38 +0530

    Fix media and static file serving in production by configuring Whitenoise finders and explicit media re_path

--- goldenfork/settings.py
+++ goldenfork/settings.py
@@ -125,6 +125,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
